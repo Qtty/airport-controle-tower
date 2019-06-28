cb4fd519e4c7c01b736ed3754cbd66b5dae579d9
melsso <melsopv@gmail.com>
2019-06-28 21:10:51 +0100

    planes rak fahem

--- Avion.java
+++ Avion.java
@@ -4,31 +4,27 @@ import com.usthb.modeles.TypeMoyen;
 
 public class Avion extends MoyenTransport{
 
-    private String compagnie, type1, numeroSequentiel1;
     private int carburant, nombreVoyageurs;
     private static int n = 0;
+    private leType typeAvion;
 
-    public Avion(long numeroSerie, String modele, String entreprise,TypeMoyen type ,leType type1,int carburant, int nombreVoyageurs){
+    public Avion(long numeroSerie, String modele, String entreprise,TypeMoyen type ,leType typeAvion,int carburant, int nombreVoyageurs){
         
         super(numeroSerie,"",modele,entreprise,type);
-        this.compagnie = compagnie;
         this.carburant = carburant;
+        this.typeAvion = typeAvion;
         this.nombreVoyageurs = nombreVoyageurs;
-        this.numeroSequentiel1 = String.format("%s%03d",type1,n);
+        this.numeroSequentiel = String.format("AVI%03d",n);
         n++;
 
     }
 
-    public String getCompagnie(){
-        return compagnie;
-    }
-
-    public String getNumeroSequentiel1(){
-        return numeroSequentiel1;
-    }
-
     public int getCarburant(){
         return carburant;
+    }
+
+    public String getTypeAvion(){
+        return String.format("%s", typeAvion);   
     }
 
     public int getNombreVoyageurs(){
@@ -36,14 +32,28 @@ public class Avion extends MoyenTransport{
     }
 
     public String toString(){  
-        return super.toString() + String.format("\nNumero sequentiel avion: %s\nCarburant: %d\nNombre Voyageurs: %d\n",numeroSequentiel1,carburant,nombreVoyageurs);
+        return super.toString() + String.format("\nType: %s\nCarburant: %d\nNombre Voyageurs: %d\n",typeAvion,carburant,nombreVoyageurs);
+    }
+
+    public Boolean checkTypeMoyen(String type){
+        try
+        {
+            leType.valueOf(type);
+        }
+        catch(IllegalArgumentException ex)
+        {
+            System.out.println("[-] Type d'avion invalid");
+            return false;
+        }
+
+        return true;
     }
 
     public static void main(String[] args)
     {
-        Avion a = new Avion(1486,"ruski","bratan",TypeMoyen.AVI,leType.ATR, 500, 100);
+        Avion a = new Avion(1486,"ruski","bratan",TypeMoyen.AVI,leType.AIRBUS, 500, 100);
             MoyenTransport b = new MoyenTransport(71486,"171732021451","ruski","bratan",TypeMoyen.CAM);
-        Vehicule c = new Vehicule(14856,"1717320214851","ruski","bratan",TypeMoyen.CAM,"fun");
+        Vehicule c = new Vehicule(14856,"1717320214851","ruski","bratan",TypeMoyen.VOI,"fun");
                 Avion d = new Avion(14860,"ruski","bratan",TypeMoyen.AVI,leType.ATR, 500, 100);
 
 
